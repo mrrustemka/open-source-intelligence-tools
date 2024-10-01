d9f8c8701e57d60efe61e043cc8b537725ec9b6a
mrrustemka <mubarakshin1996@gmail.com>
2024-10-01 20:09:33 +0400

    implement-input-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [domain, setDomain] = useState<string>("");
+
+  function submit(event: { preventDefault: () => void }): any {
+    event.preventDefault();
+    console.log(domain);
+  }
+
+  return (
+    <div className="App">
+      <form onSubmit={submit}>
+        <input
+          type="text"
+          value={domain}
+          onChange={(e) => setDomain(e.target.value)}
+          placeholder="Enter Domain"
+        />
+        <button>Scan</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
